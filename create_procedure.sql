74117f9d22bc1ca8bc35a89512bccaba94c6976c
Tyler <tchen22618@outlook.com>
2023-03-31 23:48:42 +0800

    Add more procedure for table booking validation

--- create_procedure.sql
+++ create_procedure.sql
@@ -5,19 +5,20 @@ DROP PROCEDURE GetMaxQuantity;
 DROP PROCEDURE CancelOrder;
 DROP PREPARE GetOrderDetail;
 
+
+-- Create GetMaxQuantity procedure
 CREATE PROCEDURE GetMaxQuantity()
 SELECT MAX(order_quantity) AS 'Max Quantity in Order'
 FROM Orders;
 
 
-
+-- Create CancelOrder procedure
 CREATE PROCEDURE CancelOrder(IN ID INT)
 DELETE FROM Orders
 WHERE order_id = ID;
 
 
-
-
+-- Create GetOrderDetail prepared statement
 PREPARE GetOrderDetail FROM 
 "SELECT order_id AS OrderID, 
         order_quantity AS Quantity, 
